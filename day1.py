57afaf2eaf1090c5b544e675e2afdfb98aac7d62
shashanksingh <singhshashank3333@gmail.com>
2023-06-04 22:22:00 +0100

    day1 complete

---
+++ day1.py
@@ -0,0 +1,28 @@
+# # band generator
+#
+# print("Welcome to the band name generator")
+# CityName = input(str("Name of the city: "))
+# Petname = input(str("Whats your pet name: "))
+# print(f"Your band name is {CityName}{Petname}")
+#
+
+# Quiz for nuber change:
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+c = a
+a = b
+b = c
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
