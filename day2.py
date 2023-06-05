7b4a339a31ba61073ae0ffbdae8b12e9b650ae0f
shashanksingh <singhshashank3333@gmail.com>
2023-06-05 22:03:52 +0100

    day2 complete

---
+++ day2.py
@@ -0,0 +1,18 @@
+# Data types
+
+# String
+# integer
+# float
+# boolean34
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+two_digit_number = str(two_digit_number)
+# first = two_digit_number[0]
+# second = two_digit_number[1]
+f = int(two_digit_number[0]) + int(two_digit_number[1])
+print(f)
