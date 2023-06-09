b6e58f0002096fe8a6c0e04085fcf2be7dbc1ffe
shashanksingh <singhshashank3333@gmail.com>
2023-06-10 00:29:05 +0100

    day3 complete

---
+++ day3.py
@@ -0,0 +1,133 @@
+# values = "upper circuit"
+# print(f'{values} this life is complicated ')
+# # concatination
+# a = "zebra"
+# b = " is"
+# c = " nauty animal"
+# d = a + b + c
+# print(d)
+
+# Control Flow
+
+
+# watlev = int(input("wather level: "))
+#
+# if watlev > 80:
+#     print("over flow started")
+# else:
+#     print("yet to go")
+
+# Chiecking if number is odd or even;
+
+# valueofnum = int(input("enter the number: "))
+#
+# if valueofnum % 2 == 0:
+#     print("num is even")
+# else:
+#     print("numb is odd")
+
+
+# BMI Calculator;
+#
+#
+# Weight = float(input("input your weigh :"))
+# height = float(input("whts your height:"))
+# F_weight = round(Weight/(height*height))
+#
+# if F_weight < 18.5:
+#     print(f"Underweight {F_weight}")
+# elif F_weight >18.5 and F_weight <= 25:
+#     print("Nomarmal weight")
+# elif F_weight >25 and F_weight < 30:
+#     print("overweight")
+# elif F_weight > 30 and F_weight < 35:
+#     print("obese")
+# else:
+#     print("you are clinically obese")
+#
+#
+
+
+
+# Leap Year
+# year = 2001
+# if year % 4 ==0:
+#     if year % 100 ==0:
+#         if year % 400 ==0:
+#             print("year is leap year")
+#         else:
+#             print("year is no leap year")
+#     else:
+#         print("Yr is leao year")
+# else:
+#     print("year not a leap year")
+
+
+#
+# height = int(input("height"))
+# age = int(input("whats your age"))
+# if height > 12:
+#     print("ready to ride")
+#     if age <= 12:
+#         print("pay 3$")
+#     elif age <=12 and age <=15:
+#         print("ticket is 5$")
+#     else:
+#         print("ticket is 10$")
+#     wantsphoto = input("yes or no")
+#     if wantsphoto == "yes":
+#         print("pay ")
+# else:
+#     print("grow your height bitch")
+#
+#
+# order_pizza = input("Whats your pizza S, M , L: ")
+# if order_pizza == "S":
+#     # print("bill is 15$ ")
+#     bill = 15
+# if order_pizza == "M":
+#     # print("bill is 20$ ")
+#     bill = 20
+# if order_pizza == "L":
+#     # print("bill is 25$ ")
+#     bill = 25
+#
+# peproni = input("want pepronoo Y or N")
+# if order_pizza == "S" and peproni == "Y":
+#     bill += 2
+# elif bill:
+# else order_pizza == "M" or order_pizza == "Y" and peproni == "Y":
+#      bill += 3
+#     wnatscheese = input("want chhze Y or N ")
+#     if wnatscheese == "Y":
+#      bill += 1
+#     else:
+#         bill
+# print(bill)
+# Love calculator:
+#
+# your_name = input("first name : ")
+# partner_name = input("partner name: ")
+# love = "love"
+# truelove = "true"
+# l = your_name.count("l")
+# o = your_name.count("o")
+# v = your_name.count("v")
+# e = your_name.count("e")
+# t = partner_name.count("t")
+# r = partner_name.count("r")
+# u = partner_name.count("u")
+# e = partner_name.count("e")
+# lovesum = l+o+v+e
+# truelo = t+r+u+e
+# print(lovesum)
+# print(truelo)
+# truluve_percentage = str(lovesum) + str(truelo)
+# print(truluve_percentage)
+
+
+# Treasure Island
+
+
+
+
