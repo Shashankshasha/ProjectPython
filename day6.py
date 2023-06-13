b831f850634b212587a89648109b6219714b6dde
shashanksingh <singhshashank3333@gmail.com>
2023-06-13 13:54:19 +0100

    day change start

--- day6.py
+++ day6.py
@@ -19,3 +19,47 @@
 #     abz.append(abc)
 #
 # print(abz)
+
+# a = int(input())
+# b = int(input())
+# while a > b:
+#     print("rama rama")
+# else:
+#     print("this is joke ")
+#
+
+
+# import random
+# a = random.randint(1, 100)
+# a1 = random.randint(1, 79)
+# while a > a1:
+#     print("rama rama")
+#     if a > a1:
+#         print("infinite loop ")
+#         break
+# else:
+#     print("this is joke ")
+
+
+
+
+import random
+
+def shout():
+    a = random.randint(1, 100)
+    b = random.uniform(1, 5)
+    print(f"{a}+{b}")
+
+
+shout()
+
+ab = random.randint(1, 200)
+while ab > 100:
+    shout()
+    if ab > 50:
+        break
+
+
+
+
+
