68748897cdbe11355d4f383c68ef4759ac5d6991
shashanksingh <singhshashank3333@gmail.com>
2023-06-13 12:41:23 +0100

    day change start

---
+++ day6.py
@@ -0,0 +1,21 @@
+#Defining and calling the Python functions:
+
+# creating my function:
+#
+# def addition(a, b):
+#     c = a + b
+#     # return c
+#     print(c)
+#
+# addition(2, 3)
+#
+# # learned loops
+# import random
+#
+#
+# abz = []
+# for ii in range(1, 5):
+#     abc =random.randint(1, 100)
+#     abz.append(abc)
+#
+# print(abz)
