ffe107faf9e7d0252cbfec5a754881a6be809af3
shashanksingh <singhshashank3333@gmail.com>
2023-06-13 12:40:20 +0100

    day change start

--- day5.py
+++ day5.py
@@ -1,1 +1,53 @@
-1
+# Password Generator
+
+
+# average student height:
+
+# import random
+# studentavg = []
+# for ii in range(0, 10):
+#     valgen = random.randint(100, 1000)
+#     studentavg.append(valgen)
+# print(studentavg)
+#
+# summ = 0
+# for i in studentavg:
+#     summ = summ + i
+# print(summ)
+# count_of_stulen = len(studentavg)
+# average = summ/count_of_stulen
+# print(average)
+
+
+# import random
+# highscore = []
+# for ii in range(10, 100, 10):
+#     valgen = random.randint(1, 100)
+#     highscore.append(valgen)
+# print(highscore)
+#
+# highest_score = 0
+# for i in highscore:
+#     if i > highest_score:
+#         highest_score = i
+#
+# print(highest_score)
+#
+
+toto = 0
+for i in range(0, 102, 2):
+    if i % 2 == 0:
+        toto += i
+print(toto)
+
+
+import random
+
+
+abz = []
+for ii in range(1, 5):
+    abc =random.randint(1, 100)
+    abz.append(abc)
+
+print(abz)
+
